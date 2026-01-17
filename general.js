8e2bfb6823c0256c31274c7b4312590d351d0b30
sahiljaiswal17 <sahiljaiswal17@outlook.com>
2026-01-18 01:21:55 +0530

    Update general.js

--- general.js
+++ general.js
@@ -1,42 +1,84 @@
 const axios = require("axios");
 
+/**
+ * Task 11:
+ * Retrieve all books
+ */
 exports.getAllBooks = async (req, res) => {
   try {
     const response = await axios.get("http://localhost:5000/books");
-    res.json(response.data);
+
+    if (!response.data || Object.keys(response.data).length === 0) {
+      return res.status(404).json({ message: "No books found" });
+    }
+
+    res.status(200).json(response.data);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
+/**
+ * Task 11:
+ * Retrieve book details by ISBN
+ */
 exports.getBookByISBN = async (req, res) => {
   try {
+    const isbn = req.params.isbn;
+
     const response = await axios.get(
-      `http://localhost:5000/books/isbn/${req.params.isbn}`
+      `http://localhost:5000/books/isbn/${isbn}`
     );
-    res.json(response.data);
+
+    if (!response.data || Object.keys(response.data).length === 0) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    res.status(200).json(response.data);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
+/**
+ * Task 11:
+ * Retrieve books by author
+ */
 exports.getBooksByAuthor = async (req, res) => {
   try {
+    const author = req.params.author;
+
     const response = await axios.get(
-      `http://localhost:5000/books/author/${req.params.author}`
+      `http://localhost:5000/books/author/${author}`
     );
-    res.json(response.data);
+
+    if (!response.data || Object.keys(response.data).length === 0) {
+      return res.status(404).json({ message: "No books found for this author" });
+    }
+
+    res.status(200).json(response.data);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
+/**
+ * Task 11:
+ * Retrieve books by title
+ */
 exports.getBooksByTitle = async (req, res) => {
   try {
+    const title = req.params.title;
+
     const response = await axios.get(
-      `http://localhost:5000/books/title/${req.params.title}`
+      `http://localhost:5000/books/title/${title}`
     );
-    res.json(response.data);
+
+    if (!response.data || Object.keys(response.data).length === 0) {
+      return res.status(404).json({ message: "No books found with this title" });
+    }
+
+    res.status(200).json(response.data);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
