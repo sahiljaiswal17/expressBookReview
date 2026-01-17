b8dab28f27ac95e101a19451ee6c965f4657be6f
sahiljaiswal17 <sahiljaiswal17@outlook.com>
2026-01-18 00:59:58 +0530

    Update general.js

--- general.js
+++ general.js
@@ -1,21 +1,43 @@
-const axios = require('axios');
+const axios = require("axios");
 
 exports.getAllBooks = async (req, res) => {
-  const response = await axios.get("http://localhost:5000/books");
-  res.json(response.data);
+  try {
+    const response = await axios.get("http://localhost:5000/books");
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 exports.getBookByISBN = async (req, res) => {
-  const response = await axios.get(`http://localhost:5000/books/isbn/${req.params.isbn}`);
-  res.json(response.data);
+  try {
+    const response = await axios.get(
+      `http://localhost:5000/books/isbn/${req.params.isbn}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 exports.getBooksByAuthor = async (req, res) => {
-  const response = await axios.get(`http://localhost:5000/books/author/${req.params.author}`);
-  res.json(response.data);
+  try {
+    const response = await axios.get(
+      `http://localhost:5000/books/author/${req.params.author}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 exports.getBooksByTitle = async (req, res) => {
-  const response = await axios.get(`http://localhost:5000/books/title/${req.params.title}`);
-  res.json(response.data);
+  try {
+    const response = await axios.get(
+      `http://localhost:5000/books/title/${req.params.title}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
